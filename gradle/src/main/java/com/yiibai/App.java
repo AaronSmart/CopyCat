3dc32bbfeee61cd6fce85691cd71bcf0c73ea4c9
Aaron <38056426+AaronSmart@users.noreply.github.com>
2021-10-11 20:39:25 +0800

    Delete gradle directory

--- gradle/src/main/java/com/yiibai/App.java
+++
@@ -1,8 +0,0 @@
-package com.yiibai;
-public class App
-{
-	public static void main(String []args)
-	{
-		System.out.println("hello world this is the test of jenkins,never fuck up ");
-	}
-}
--- gradle/src/test/java/com/yiibai/AppTest.java
+++
@@ -1,9 +0,0 @@
-package com.yiibai;
-public class AppTest
-{
-   public static void main(String []args)
-   {
-	   System.out.println("hello this is Aaron Smart ,work to the jenkins");
-   }
-}
-	
